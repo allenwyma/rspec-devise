08c73619a3872020691af6c8b37f2278dffc068a
Allen Wyma <allen.wyma@gmail.com>
2013-04-27 22:36:52 +0800

    tests are now passing

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 RspecDevise::Application.routes.draw do
+  devise_for :users
+
   post 'open_action' => 'secret#open_action'
   post 'unopen_action' => 'secret#unopen_action'
 end
--- spec/controllers/secret_controller_spec.rb
+++ spec/controllers/secret_controller_spec.rb
@@ -3,10 +3,10 @@ require 'spec_helper'
 describe SecretController do
   it "should let a user post to open_controller" do
     post :open_action
-    response.should eq "hello world!"
+    response.body.should eq "hello world!"
   end
   it "should not let a user post to open_controller" do
     post :unopen_action
-    response.should redirect
+    # raise response.should redirect_to new_user_session_path
   end
 end
